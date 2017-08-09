e1f1ddc87933717607630c90d3aa6e9df0efdddf
yaoyin001 <492680716@qq.com>
2017-08-09 17:30:59 +0800

    0809

--- blue/js/onepage_index.js
+++ blue/js/onepage_index.js
@@ -12,7 +12,7 @@
 	var tit=document.querySelectorAll(".title1 a");
 	xmlhttp.onreadystatechange=function(){
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-//			console.log("后台数据"+xmlhttp.responseText);
+			console.log("后台数据"+xmlhttp.responseText);
 			var s=JSON.parse(xmlhttp.responseText);
 //			console.log(s.isSuccess);
 			for(var i=0;i<tit.length;i++){
@@ -39,13 +39,17 @@
 	var head_p4=document.querySelector(".projects_title p:nth-of-type(3)");
 	xmlhttp1.onreadystatechange=function(){
 		if(xmlhttp1.readyState==4 && xmlhttp1.status==200){
-//			console.log(xmlhttp1.responseText);
+			console.log(xmlhttp1.responseText);
 			var s=JSON.parse(xmlhttp1.responseText);
-//			console.log(s+"head_font的数据");
+			console.log(s+"head_font的数据");
 			head_p1.innerText=s.result[0].name;
 			head_p2.innerText=s.result[0].describe;
 			head_p3.innerText=s.result[0].projectsTitle;
 			head_p4.innerText=s.result[0].projectsDescribe;
+			
+			var img=s.result[0].img;
+			console.log(img);
+			document.querySelector(".head").backgroundImage="url(img)";
 		}
 	}
 	
@@ -63,15 +67,19 @@
 	xmlhttp2.send();
 	//class为projects_transition的数据
 	var pro_transion=document.querySelectorAll(".projects_transition");
+	var pro_ul_img=document.querySelectorAll(".projects_ul li img");
+	console.log(pro_ul_img.length);
 	xmlhttp2.onreadystatechange=function(){
 		if(xmlhttp2.readyState==4 && xmlhttp2.status==200){
 			console.log(xmlhttp2.responseText);
 			var s=JSON.parse(xmlhttp2.responseText);
 			for(var i=0;i<pro_transion.length;i++){
-				console.log(pro_transion[i].querySelector('p:nth-of-type(1)'))
 				pro_transion[i].querySelector('p:nth-of-type(1)').innerText=s.result[i].title;
 				pro_transion[i].querySelector('p:nth-of-type(2)').innerText=s.result[i].subtitle;
+				pro_ul_img[i].src=s.result[i].bgimg;
 			}
+			
+			
 		}
 	}
 	
@@ -99,5 +107,11 @@
 			fixblue_p1.innerHTML=s.result[0].title;
 			fixblue_p3.innerHTML=s.result[0].name;
 			fixblue_p4.innerHTML=s.result[0].describe;
+			
+			//插入头像数据
+			var img=s.result[0].header;
+			var img1=s.result[0].bgimg;
+			document.querySelector(".fixed").backgroundImage="url(img1)";
+			document.querySelector(".fixed_circle").backgroundImage="url(img)";
 		}
 	}
