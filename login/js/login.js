46b843ee63d675102e4f80eda6a15c5acf759622
yaoyin001 <492680716@qq.com>
2017-08-16 11:21:07 +0800

    0816注册页面修改

--- login/js/login.js
+++ login/js/login.js
@@ -1,3 +1,4 @@
+	//登录按钮功能
 	document.querySelector(".content button").addEventListener("click",function(){
 		var xmlhttp;
 		if (window.XMLHttpRequest) {
@@ -12,17 +13,27 @@
 		xmlhttp.setRequestHeader("Content-Type","application/JSON");
 		var user={
 			phone:account,
-			password:pwd
+			password:hex_md5(pwd)
 		};
 		xmlhttp.send(JSON.stringify(user));
-		xmlhttp.onreadystatechange=function(e){
+		xmlhttp.onreadystatechange=function(){
 			if(xmlhttp.readyState==4 && xmlhttp.status==200){		
-				console.log(xmlhttp.responseText);
-				window.location.assign( "http://192.168.0.115:8020/login/mobile_JT.html?__hbt=1502712522078"); 
+				var islog=JSON.parse(xmlhttp.responseText);
+				//判断登录成功和失败
+				if(islog.islogin==true){
+					alert("登录成功，欢迎"+account+"回来！^_^");
+					location.href="http://192.168.0.115:8020/login/mobile_JT.html"; 
+				}else{
+					alert("你输入的账户名或者密码错误，请重新输入！");
+					location.reload(); 
+				}
 			}
-			
 		}
-
+	})
+	
+	//登录界面的注册按钮功能
+	document.querySelector(".register").addEventListener("click",function(){
+		location.href="http://192.168.0.115:8020/login/register.html";
 	})
 	
 	
