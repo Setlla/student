a29d032a846cd6d565721b53374086e452938c19
D1014520 <D1014520@126.com>
2017-08-17 12:26:45 +0800

    dy8.17

--- javascript/blue/js/zhuce8.15.js
+++ javascript/blue/js/zhuce8.15.js
@@ -1,16 +1,14 @@
-var email=document.querySelector(".one1");
-var phone=document.querySelector(".one2");
 var password=document.querySelector(".two");
 var rgr = document.querySelector(".register");
 var ph = document.querySelector(".phone");
 var	em = document.querySelector(".email");
 var pass = document.querySelector(".password");
-var isphone=false;
+var isphone=true;
 	rgr.addEventListener("click",function(e){
 	   e.target.style.borderBottom = "3px solid #33ccff";
 	 if(e.target.className=="emails"){
-	 	phone.style.display="none";
-	 	email.style.display="block"; 
+	 	ph.parentNode.style.display="none";
+	 	em.parentNode.style.display="block"; 
 	 	console.log(e.target);
 	 	e.target.previousElementSibling.style.borderBottom="none";
 	 	isphone=false;
@@ -18,51 +16,62 @@ var isphone=false;
 	   else{
 	   	console.log(e.target);
 	   	e.target.nextElementSibling.style.borderBottom="none";
-	   	email.style.display="none";
-	   	phone.style.display="block";
+	   	em.parentNode.style.display="none";
+	   	ph.parentNode.style.display="block";
 	   	isphone=true;
 	   }
 })
-	var check = function() {
-		if(isphone) {
-			viliphone(ph.value);
-		}else
-		{
-			viliemail(em.value);
-		}
-	}
-	
+//	var check = function() {
+//		if(isphone) {
+//			viliphone(ph.value);
+//		}else{
+//			viliemail(em.value);
+//		}
+//	}
+
+
+
+
+var xhr =new XMLHttpRequest;
 var register = document.querySelector(".foot");
 	register.addEventListener("click",function(){	
-	check();	
-	
-	if(window.XMLHttpRequest){
-		xhr=new XMLHttpRequest();
-	}
-		else{
-			xhr=new ActiveXObject();
+	if(isphone){
+		if(viliphone(ph.value)){
+			submit();
 		}
-	
-	var xhr =new XMLHttpRequest;
-	var user ={
-		phone:ph.value,
-		email:em.value,
-		passward:hex_md5(pass.value)
+	}else{
+		if(viliemail(em.value)){
+			submit();
+		}
 	}
-	xhr.open('post','http://192.168.0.127:3900/reg');
-	xhr.setRequestHeader("Content-Type","application/json");
-	xhr.send(JSON.stringify(user));
-	xhr.onreadystatechange=function(){
-		if(xhr.readyState==4 && xhr.status==200){
-			var result=JSON.parse(xhr.responseText);
-			if(result.isSuccess==true){
-				location.href="8.13.html";
-			}else{
-				console.log(result);
+})
+	function submit(){
+		if(window.XMLHttpRequest){
+			xhr=new XMLHttpRequest();
+		}
+			else{
+				xhr=new ActiveXObject();
+			}
+		var user ={
+			phone:ph.value,
+			email:em.value,
+			passward:hex_md5(pass.value)
+		}
+		xhr.open('post','http://192.168.0.127:3900/reg');
+		xhr.setRequestHeader("Content-Type","application/json");
+		xhr.send(JSON.stringify(user));
+		xhr.onreadystatechange=function(){
+			if(xhr.readyState==4 && xhr.status==200){
+				var result=JSON.parse(xhr.responseText);
+				if(result.isSuccess==true){
+					location.href="8.13.html";
+				}else{
+					console.log(result);
+				}
 			}
 		}
 	}
-	})
+
 	
 		
 	
