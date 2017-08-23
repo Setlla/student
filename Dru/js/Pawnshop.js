cfd5054cf5b10f7593983e118997313a0b6d41d5
朱志湖 <369323586@qq.com>
2017-08-23 20:55:04 +0800

    最新版本

--- Dru/js/Pawnshop.js
+++ Dru/js/Pawnshop.js
@@ -10,27 +10,30 @@ $.ajax({
 })
 
 	$(document).on("click",".qh p",function	()	{
-		if (this.className=="left_click") {
-			$(".right_click").css("background","#33CCFF");
-			$(".left_click").css("background","#D6F5FF");
-			$(".content_three").hide();
-			$(".content_two").show();
-		} else{
-			$(".right_click").css("background","#D6F5FF");
-			$(".left_click").css("background","#33CCFF");
-			$(".content_three").show();
-			$(".content_two").hide();			
-		}
-	})
+		$(this).addClass("cur").siblings().removeClass("cur");  //点击当前添加标签CUR。所有兄弟元素删除标签CUR。
+		$(".content").eq($(this).index()).show();		//选择点击对应的内容显示
+		$(".content").eq($(this).index()).siblings(".content").hide();	//拿到当前内容块，另外内容块隐藏。
+
+
+//		if (this.className=="left_click") {
+//			$(".right_click").css("background","#33CCFF");
+//			$(".left_click").css("background","#D6F5FF");
+//			$(".content_three").hide();
+//			$(".content_two").show();
+//		} else{
+//			$(".right_click").css("background","#D6F5FF");
+//			$(".left_click").css("background","#33CCFF");
+//			$(".content_three").show();
+//			$(".content_two").hide();			
+//		}
+})
 
 
 
-
+		//内容拼接。
 	function setProduct(result){
-		
-		
+				
 		for(var i=0;i<result.length;i++){
-
 			var	textContent='<div class="content_noe">'
 					+'<img src="'+result[i].Image+'"/ width="180" height="140">'
 					+'<div class="right_one">'
@@ -45,16 +48,20 @@ $.ajax({
 						+'<a></a>'
 						+'<a></a>'
 					+'</div>'
+
+
+			
+//			var node=document.createElement("div");  //创造一个元素DIV 
+////			node.setAttribute("class","textContent");  //设置一个属性textContent
+//			node.innerHTML=textContent;			//选取HTML 的内容 
+//////			textContent.innerHTML=node;
 				
-			var node=document.createElement("div");  //创造一个元素DIV 
-//			node.setAttribute("class","textContent");  //设置一个属性textContent
-			node.innerHTML=textContent;			//选取HTML 的内容 
-////			textContent.innerHTML=node;
 				
+				//判断 。
 			if(result[i].IsBook==0){
-				$(".content_two").append(node);
+				$(".content_two").append(textContent);
 			}else{
-				$(".content_three").append(node);
+				$(".content_three").append(textContent);
 			}
 		}
 	}
