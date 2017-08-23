e14c169b02aff697b0585da9655e8e256be27bea
lipei19931108 <l>
2017-08-23 20:49:09 +0800

    8.23

--- register/js/click.js
+++ register/js/click.js
@@ -1,22 +1,41 @@
-var thing=document.querySelector(".thing");
-var books=document.querySelector(".books");
-var bton=document.querySelector(".bton");
-var res=document.querySelector(".res");
-var book=document.querySelector(".book");
-  bton.addEventListener('click',function(e){
-  	if(e.target.innerHTML=="物品"){
-  		res.style.background="#d6f5ff";
-  		book.style.background="none";
-  		res.style.color="#33ccff";
-  		book.style.color="#FFFFFF"
-  		thing.style.display="block";
-  		books.style.display="none";
-  	}else{
-  		book.style.background="#d6f5ff";
-  		res.style.background="none";
-  		res.style.color="#FFFFFF";
-  		book.style.color="#33ccff"
-  		thing.style.display="none";
-  		books.style.display="block";
-  	}
-  })
+$(document).ready(function(){
+		var thing=$("div.thing");
+		var books=$("div.books");
+		var bton=$("div.bton");
+		var res=$("a.res");
+		var book=$("a.book");
+bton.click(function(e){
+		 if(e.target.innerHTML=="物品"){
+		 	 res.css("background-color","#d6f5ff");
+		 	 book.css("background-color","#33ccff");
+		 	 res.css("color","#33ccff");
+		 	 book.css("color","#FFFFFF");
+		 	 thing.css("display","block");
+		 	 books.css("display","none");
+		 }else{
+		 	res.css("background-color","#33ccff");
+		 	book.css("background-color","#d6f5ff");
+		 	res.css("color","#FFFFFF");
+		 	book.css("color","#33ccff");
+		 	thing.css("display","none");
+		 	books.css("display","block");
+		 }
+	});
+});
+//bton.addEventListener('click',function(e){
+//	if(e.target.innerHTML=="物品"){
+//		res.style.background="#d6f5ff";
+//		book.style.background="none";
+//		res.style.color="#33ccff";
+//		book.style.color="#FFFFFF"
+//		thing.style.display="block";
+//		books.style.display="none";
+//	}else{
+//		book.style.background="#d6f5ff";
+//		res.style.background="none";
+//		res.style.color="#FFFFFF";
+//		book.style.color="#33ccff"
+//		thing.style.display="none";
+//		books.style.display="block";
+//	}
+//})
--- register/js/dp.js
+++ register/js/dp.js
@@ -1,44 +1,51 @@
-var xhr;
-	if(window.XMLHttpRequest){
-		xhr=new XMLHttpRequest();
-	}else{
-		xhr=new ActiveXObject();
-	}
-	
-	xhr.open('POST','http://192.168.0.146:3900/productList',true);
-	xhr.send();
-	
-	xhr.onreadystatechange=function(){
-		 	if(xhr.readyState==4 && xhr.status==200){
-		 		var result = JSON.parse(xhr.responseText);
-	            console.log(result)
-	            
-	            for(var i=0;i<result.result.length;i++ ){
-	            	var resContent = '<div class="bike">'
-	            					+ '<img class="img" src="'+ result.result[i].Image +'" />'
-	            					+ '<p class="title">'+result.result[i].Des +'</p>'
-	            					+ '<span>'+ result.result[i].Carriage +'</span>'
-	            					+ '<span class="city">'+ result.result[i].Destination +'</span>'
-	            					+ '<button>'+ result.result[i].Status +'</button>'
-	            					+ '<div class="much"> '
-	            					+ '<p class="nub">'
-	            					+ '<span>￥</span>'+ result.result[i].CurPrice +'</p>'
-	            					+ '<p class="nub2">￥' + result.result[i].OldPrice +'</p>'
-	            					+ '</div>'
-	            					+ '</div>'
-	            	
-	            	
-	            	var node = document.createElement('div');
+$.post('http://192.168.0.146:3900/productList',function(data,status){
+	for(var i=0;i<data.result.length;i++ ){
+    	var resContent = '<div class="bike">'
+    					+ '<img class="img" src="'+ data.result[i].Image +'" />'
+    					+ '<p class="title">'+data.result[i].Des +'</p>'
+    					+ '<span>'+ data.result[i].Carriage +'</span>'
+    					+ '<span class="city">'+ data.result[i].Destination +'</span>'
+    					+ '<button>'+ data.result[i].Status +'</button>'
+    					+ '<div class="much"> '
+    					+ '<p class="nub">'
+    					+ '<span>￥</span>'+ data.result[i].CurPrice +'</p>'
+    					+ '<p class="nub2">￥' + data.result[i].OldPrice +'</p>'
+    					+ '</div>'
+    					+ '</div>'
+    	
+    	
+//	            	var node = document.createElement('div');
 //	            	var dx=document.querySelector(".thing");
 //	            	var shu=document.querySelector(".books")
-	            	node.innerHTML = resContent;
-	            	if(result.result[i].IsBook == "0"){
-	            		thing.appendChild(node);
-	            	}else{
-	            		books.appendChild(node);
-	            	}
-	            	
-	            }
+//	            	node.innerHTML = resContent;
+    var thing=$("div .thing");
+	var books=$("div .books");
+    	if(data.result[i].IsBook == 0){
+    		$('.thing').append(resContent);
+    	}else{
+    		$('.books').append(resContent);
+    	}
+    }
+})
+
+//var xhr;
+//	if(window.XMLHttpRequest){
+//		xhr=new XMLHttpRequest();
+//	}else{
+//		xhr=new ActiveXObject();
+//	}
+//	
+//	xhr.open('POST','http://192.168.0.146:3900/productList',true);
+//	xhr.send();
+//	
+//	xhr.onreadystatechange=function(){
+//		 	if(xhr.readyState==4 && xhr.status==200){
+//		 		var result = JSON.parse(xhr.responseText);
+//	            console.log(result)
+//	            
+	            
+//	            	
+//	            }
 //	         <div class="bike">
 //				<img class="img" src="img/dp_07.png" />
 //				<p class="title">捷安特24速变速一体轮可折叠自行车</p>
@@ -50,5 +57,5 @@ var xhr;
 //				    <p class="dot">● ● ●</p>
 //				</div>			
 //			</div>
-		 	}
-		 }
+//		 	}
+//		 }
