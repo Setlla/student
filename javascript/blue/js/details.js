ecb1530e5069448cb58f86b517d58ec12dfb48de
D1014520 <D1014520@126.com>
2017-08-25 20:56:41 +0800

    dy

--- javascript/blue/js/details.js
+++ javascript/blue/js/details.js
@@ -37,13 +37,59 @@
 					                +'<span>'+data.result.product.Destination+'</span>'
 								+'</div>'
 							+'</div>'
+							
+							
 							+'<div class="content">'
 								+'<p>宝贝描述</p>'
 								+'<p>'+data.result.product.Des+'<p>'
 							+'</div>'
 			$(".heighter").html(conpicture);
-		}	
+		}
 		
-			
 
-				
+
+
+//
+//		var length=$(".round span").length;
+//		var id=0;
+//		
+//		$(".round span").click(function(){
+//			var index=$(".round span").index(this);
+//			$(this).css("background","#33ccff").siblings().css("background","#fff");
+//			$(".book").children("img").eq(index).show().siblings("img").hide();
+//			id=index;
+//	})
+//		
+//		var a=setTimeout("Carousel()",1000); 
+//		var Carousel=function(){
+//			$(".round span").eq(id).trigger("click");
+//			id=(id+1)% length;
+//			a=setTimeout(Carousel,1000);
+//		}
+//		$(".book img").hover(function(){
+//			clearTimeout(a);
+//		},function(){
+//			a=setTimeout(Carousel,1000);
+//		})
+			
+		var length=$(".round span").length;
+		var id=0;
+		$(".round span").click(function(){
+			var index=$(".round span").index(this);
+			$(this).css("background","#33ccff").siblings().css("background","#fff");
+			$(".book").children("img").eq(index).show().siblings("img").hide();
+			id=index;
+		})
+		
+		
+		var a=setInterval("Carousel()",1000); 
+		var Carousel=function(){
+			$(".round span").eq(id).trigger("click");
+			 id=(id+1)% length;
+			
+		}
+		$(".book img").hover(function(){
+			clearInterval(a);
+		},function(){
+			a=setInterval(Carousel,1000);
+		})
