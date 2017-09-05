ffd920741614eb818d742edf4a407874f0a19801
D1014520 <D1014520@126.com>
2017-09-05 10:58:28 +0800

    dy9.5

--- daiying/js/shopcar.js
+++ daiying/js/shopcar.js
@@ -11,74 +11,73 @@ $.ajax({
 var setData=function(data){
 	for(var n=0;n<data.result.length;n++){
 		var strings='<li class="bicycle">'
-			+'<a class="round"><span class="dots"></span></a>'
-			+'<img src='+data.result[n].product.Image+' />'
-			+'<div class="right">'
-				+'<div class="top">'
+					+'<a class="round"><span class="dots"></span></a>'
+					+'<img src='+data.result[n].product.Image+' />'
+					+'<div class="right">'
+					+'<div class="top">'
 					+'<p>'+data.result[n].product.Des+'</p>'
 					+'<span>'+data.result[n].product.Carriage+'</span>'
 					+'<span>'+data.result[n].product.Destination+'</span>'
 					+'<a>'+data.result[n].product.Status+'</a>'
-				+'</div>'
-				+'<div class="undown">'
-					+'<span>￥ '+data.result[n].product.CurPrice+'</span>'
-					+'<span>价格￥：'+ data.result[n].product.OldPrice+'</span>'
-					+'<span>X2</span>'
-				+'</div>'
-			+'</div>'
-		+'</li>'
-		
-
-				$(".content").append(strings);
-		}
+					+'</div>'
+					+'<div class="undown">'
+					+'<span>￥ <i class="newPrice">'+data.result[n].product.CurPrice+'</i></span>'
+					+'<span>价格￥：<i>'+ data.result[n].product.OldPrice+'</i></span>'
+					+'<span>X<i class="number">1</i></span>'
+					+'</div>'
+					+'</div>'
+					+'</li>'
+					$(".content").append(strings);
+	}
 }
 
-var round=$(".bicycle .dots");
-	round.click(function(e){
-		if ($(this).hasClass("cur")) {
-			$(this).removeClass("cur");
-		} else{
-			$(this).addClass("cur");
-		}
-		if($(".bicycle .dots").length==$(".round .cur").length){
-			$(".dot .dots").addClass("cur");
-		}else{
-			$(".dot .dots").removeClass("cur");
-		}
+
+$(document).on("click",".bicycle .dots",function(e){	
+	if ($(this).hasClass("cur")) {
+		$(this).removeClass("cur");
+	} else{
+		$(this).addClass("cur");
+	}
+	if($(".bicycle .dots").length==$(".round .cur").length){
+		$(".dot .dots").addClass("cur");
+	}else{
+		$(".dot .dots").removeClass("cur");
+	}
 	setsum();
 	setnum();
 })
-$(".dot .dots").click(function(e){
+$(document).on("click",".dot .dots",function(e){
+	var round=$(".bicycle .dots");
 	if ($(this).hasClass("cur")) {
-			$(this).removeClass("cur");
-			round.removeClass("cur");
+		$(this).removeClass("cur");
+		round.removeClass("cur");
 		} else{
-			$(this).addClass("cur");
-			round.addClass("cur");
+		$(this).addClass("cur");
+		round.addClass("cur");
 		}
 	setsum();
 	setnum();		
 })
 
 function setsum(){
-	 	var sum=0;
-	 	var content=$(".bicycle .cur");
-	 	for (var i=0;i<content.length;i++) {
-	 		 var newPrice=parseInt(content.eq(i).parents(".bicycle").find(".newPrice").html());
-	 		 var number=parseInt(content.eq(i).parents(".bicycle").find(".number").html());
-	 		 sum=sum+newPrice*number;
-	 	}
-	 	$(".sum").html(sum);
+ 	var sum=0;
+ 	var content=$(".bicycle .cur");
+ 	for (var i=0;i<content.length;i++) {
+ 		 var newPrice=parseInt(content.eq(i).parents(".bicycle").find(".newPrice").html());
+ 		 var number=parseInt(content.eq(i).parents(".bicycle").find(".number").html());
+ 		 sum=sum+newPrice*number;
+ 	}
+ 	$(".sum").html(sum);
 
-	}
+}
 function setnum(){
-		var num=0;
-		var content=$(".bicycle .cur");		
-		for (var i=0;i<content.length;i++) {
-			var number=parseInt(content.eq(i).parents(".bicycle").find(".number").html());
-			num=num+number;
-		}
-		$(".zero").html("("+num+")");
+	var num=0;
+	var content=$(".bicycle .cur");		
+	for (var i=0;i<content.length;i++) {
+		var number=parseInt(content.eq(i).parents(".bicycle").find(".number").html());
+		num=num+number;
+	}
+	$(".zero").html("("+num+")");
 	}
 
 
