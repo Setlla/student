161109e6fb2cb57555f65406bf3fbd60cd59f26d
D1014520 <D1014520@126.com>
2017-09-05 09:29:21 +0800

    9.5

--- daiying/js/details.js
+++ daiying/js/details.js
@@ -106,7 +106,7 @@ $(".shopcar").click(function(){
 	async:true,
 	contentType:"application/json",
 	data:JSON.stringify({
-		token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.W3siaWQiOjM1LCJwaG9uZSI6IjEzMDM2NzQ1NjM5XG4iLCJlbWFpbCI6bnVsbCwicGFzc3dvcmQiOiIxMjM0NTYiLCJoZWFkSW1hZ2UiOm51bGwsIm5hbWUiOm51bGwsImdlbmRlciI6bnVsbCwiYWRkcmVzcyI6bnVsbCwiY3JlYXRlZEF0IjoiMjAxNy0wOS0wMlQwNjozNDo0MS4wMDBaIiwidXBkYXRlZEF0IjoiMjAxNy0wOS0wMlQwNjozNDo0MS4wMDBaIn1d.XfBj5-WvZDFtNGEP3B7e7OtFthlS_og8zDNL-KDTWfA",
+		token:localStorage.getItem("token"),
 		id: getParams('id')
 	}),
 	success:function(data){
--- daiying/js/shopcar.js
+++ daiying/js/shopcar.js
@@ -1,10 +1,87 @@
 $.ajax({
 	type:"post",
-	url:"http://39.108.219.59/getShopCa",
+	url:"http://39.108.219.59/getShopCar",
 	async:true,
-	data:
-	sucess:
+	contentType:"application/json",
+	data:JSON.stringify({token:localStorage.getItem("token")}),
+	success:function(data){
+		setData(data);
+	}
 });
-$(".dot").click(function(){
-	
+var setData=function(data){
+	for(var n=0;n<data.result.length;n++){
+		var strings='<li class="bicycle">'
+			+'<a class="round"><span class="dots"></span></a>'
+			+'<img src='+data.result[n].product.Image+' />'
+			+'<div class="right">'
+				+'<div class="top">'
+					+'<p>'+data.result[n].product.Des+'</p>'
+					+'<span>'+data.result[n].product.Carriage+'</span>'
+					+'<span>'+data.result[n].product.Destination+'</span>'
+					+'<a>'+data.result[n].product.Status+'</a>'
+				+'</div>'
+				+'<div class="undown">'
+					+'<span>￥ '+data.result[n].product.CurPrice+'</span>'
+					+'<span>价格￥：'+ data.result[n].product.OldPrice+'</span>'
+					+'<span>X2</span>'
+				+'</div>'
+			+'</div>'
+		+'</li>'
+		
+
+				$(".content").append(strings);
+		}
+}
+
+var round=$(".bicycle .dots");
+	round.click(function(e){
+		if ($(this).hasClass("cur")) {
+			$(this).removeClass("cur");
+		} else{
+			$(this).addClass("cur");
+		}
+		if($(".bicycle .dots").length==$(".round .cur").length){
+			$(".dot .dots").addClass("cur");
+		}else{
+			$(".dot .dots").removeClass("cur");
+		}
+	setsum();
+	setnum();
 })
+$(".dot .dots").click(function(e){
+	if ($(this).hasClass("cur")) {
+			$(this).removeClass("cur");
+			round.removeClass("cur");
+		} else{
+			$(this).addClass("cur");
+			round.addClass("cur");
+		}
+	setsum();
+	setnum();		
+})
+
+function setsum(){
+	 	var sum=0;
+	 	var content=$(".bicycle .cur");
+	 	for (var i=0;i<content.length;i++) {
+	 		 var newPrice=parseInt(content.eq(i).parents(".bicycle").find(".newPrice").html());
+	 		 var number=parseInt(content.eq(i).parents(".bicycle").find(".number").html());
+	 		 sum=sum+newPrice*number;
+	 	}
+	 	$(".sum").html(sum);
+
+	}
+function setnum(){
+		var num=0;
+		var content=$(".bicycle .cur");		
+		for (var i=0;i<content.length;i++) {
+			var number=parseInt(content.eq(i).parents(".bicycle").find(".number").html());
+			num=num+number;
+		}
+		$(".zero").html("("+num+")");
+	}
+
+
+
+
+
