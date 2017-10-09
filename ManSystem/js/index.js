81bbacedca4edc03f9ef859d792136debebea012
yaoyin <492680716@qq.com>
2017-10-09 14:18:40 +0800

    优化

--- ManSystem/js/index.js
+++ ManSystem/js/index.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 		if ($(this).children("ul").hasClass("hide")) {
 			$(this).children("ul").removeClass("hide");
 			$(this).parents(".managelist").find(".arr").css("transform","translate(0,-50%) rotate(315deg)")
-		} else{
+		} 
+		else{
 			$(this).children("ul").addClass("hide");
 			$(this).parents(".managelist").find(".arr").css("transform","translate(0,-50%) rotate(225deg)")
 		}
