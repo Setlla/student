f2ee95214bd20188c0ef8b76fbc13557e5fbfaba
yaoyin <492680716@qq.com>
2017-10-18 19:28:01 +0800

    渲染

--- yaoyin/js/order_list.js
+++ yaoyin/js/order_list.js
@@ -1,24 +1,5 @@
 	$(document).ready(function(){
 		//搜索功能
-		var vm_search=new Vue({
-			el:".search",
-			data:{
-				isSearch:false,
-				isSer:false
-			},
-			methods:{
-				search_ipt1:function(){
-					this.isSearch=true;
-					this.isSer=true;
-				},
-				search_ipt2:function(){
-					this.isSearch=false;
-					this.isSer=false;
-				}
-			}
-			
-			
-		})	
 //		$(document).on("change",".search_ipt",function(){
 //			$(".warelist").empty();
 //			romance();
@@ -33,41 +14,18 @@
 //	        }  
 //  	}) 
 		
-		
-		//数据渲染
-		var warelist=new Vue({
-			el:".warelist",
-			data:{
-				productName:'',
-				items:[]
-			},
-			created:function(){
-				var that=this;
-				axios.post(_url+"/getOrder",{
-					token:localStorage.getItem("token"),
-					productName:productName
-				})
-				.then(function(response){
-					console.log(response.data);
-					that.items=response.data.result;
-				})
-				.catch(function(error){
-					console.log(error);
-				})
-			}
-		})
 		//数据渲染父组件
 		Vue.component('father',{
 			props:["items"],
 			template:'<div>'
-					+'<div class="wl" v-for="item in items">'
+					+'<div class="wl" v-for="item in items" :data-id="item.id">'
 					+'<div class="monopoly">'
 		        	+'<img src="img/icon/icon_63.png"/>'
 		        	+'<span>君宝话费充值专营店</span>'
 		        	+'<span class="mon_arr"></span>'
 		        	+'<span class="mon_success">交易成功</span>'
 		       		+'</div>'
-					+'<child :products="item.products" v-on:item="productNum"></child>'
+					+'<child :products="item.products" :product-num="JSON.parse(item.productNum)"></child>'
 		        	+'<ul class="recharge_2">'
 			    	+'<li>'
 	        		+'<span>共</span><span>{{item.totalNum}}</span><span>件商品</span>'
@@ -85,9 +43,9 @@
 		
 		//数据渲染子组件
 		Vue.component('child',{
-			props:["products"],
+			props:["products","productNum"],
 			template:'<div>'
-					+'<ul class="recharge_1" v-for="product in products">'
+					+'<ul class="recharge_1" v-for="(product,index) in products">'
 		        	+'<li>'
 		        	+'<img :src="product.Image"/>'
 		        	+'</li>'
@@ -97,11 +55,46 @@
 		        	+'<li>'
 			        +'<p><span>￥</span><span class="new_price">{{product.CurPrice}}</span></p>'
 			        +'<p><span>￥</span><span class="old_price">{{product.OldPrice}}</span></p>'
-			        +'<p>X<span class="num">22</span></p>'
+			        +'<p>X<span class="num">{{ productNum[index] }}</span></p>'
 		        	+'</li>'
 		      		+'</ul>'
 		      		+'</div>'
 		})
+		//数据渲染
+		var warelist=new Vue({
+			el:".warelist",
+			data:{
+				productName:'',
+				isSearch:false,
+				isSer:false,
+				items:[]
+			},
+			methods:{
+				search_ipt1:function(){
+					this.isSearch=true;
+					this.isSer=true;
+				},
+				search_ipt2:function(){
+					this.isSearch=false;
+					this.isSer=false;
+				}
+			},
+			created:function(){
+				var that=this;
+				axios.post(_url+"/getOrder",{
+					token:localStorage.getItem("token"),
+					productName:this.productName
+				})
+				.then(function(response){
+					console.log(response.data);
+					that.items=response.data.result;
+				})
+				.catch(function(error){
+					console.log(error);
+				})
+			}
+		})
+
 		
 		//删除订单
 		$(document).on("click",".del_btn",function(){
